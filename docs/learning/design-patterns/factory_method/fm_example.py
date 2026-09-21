e4c87f1a2573b5e1d00d59fb2f16968c96fe57d8
jayrajan <jrajan@jprtech.com>
2026-09-21 07:22:08 +0100

    feat: implementing speed limits logic for each trasport products

--- docs/learning/design-patterns/factory_method/fm_example.py
+++ docs/learning/design-patterns/factory_method/fm_example.py
@@ -5,6 +5,7 @@
 
 # Product Interface
 from abc import ABC, abstractmethod
+from turtle import mode
 
 
 class TransportMode(ABC):
@@ -14,30 +15,48 @@ class TransportMode(ABC):
     @abstractmethod
     def OperationMode(self, message: str) -> str:
         pass
+    @abstractmethod
+    def SpeedLimit(self, speed: int ) -> str:
+        pass
 
 # Various implementation of the TransportMode interface
 class TransportModeAir(TransportMode):
     def OperationMode(self, message: str) -> str:
         return f"{message}, Flying in the air"
+    def SpeedLimit(self, speed):
+        return f"Speed limit is {speed} km/h for air transport"
 
 class TransportModeLand(TransportMode):
     def OperationMode(self, message: str) -> str:
         return f"{message}, Driving on the land"
+    def SpeedLimit(self, speed):
+        return f"Speed limit is {speed} km/h for land transport"
 
 class TransportModeWater(TransportMode):
     def OperationMode(self, message: str) -> str:
         return f"{message}, Sailing on the water"
+    def SpeedLimit(self, speed):
+        return f"Speed limit is {speed} km/h for water transport"
 
 
 # Create Transportation 
 class TransportationCreator(ABC):
+    # THis the factory method that determines which 
+    # product objects gets created. The factory method is 
+    # abstract and must be implemented by the subclasses.
     @abstractmethod
     def create_transportation(self) -> TransportMode:
         pass
 
+    def create_transportation_ctx(self) -> TransportMode:
+        return self.create_transportation()
+
+    
     def output_mode_status(self, message: str) -> None:
-        mode = self.create_transportation()
-        print(mode.OperationMode(message))
+        print(self.create_transportation_ctx().OperationMode(message))
+
+    def output_speed_limit(self, speed: int) -> None:
+        print(self.create_transportation_ctx().SpeedLimit(speed))
 
 class AirTransportationCreator(TransportationCreator):
     def create_transportation(self) -> TransportMode:
@@ -53,9 +72,12 @@ class WaterTransportationCreator(TransportationCreator):
 
 air_transportation_creator = AirTransportationCreator()
 air_transportation_creator.output_mode_status("Hello from air")
+air_transportation_creator.output_speed_limit(800)
 
 land_transport_handler = LandTransportationCreator()
 land_transport_handler.output_mode_status("Hello from land")
+land_transport_handler.output_speed_limit(120)
 
 water_transport_handler = WaterTransportationCreator()
 water_transport_handler.output_mode_status("Hello from water")
+water_transport_handler.output_speed_limit(60)
